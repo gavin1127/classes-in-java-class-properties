bc9527c40a46e1be209e356d624049d8d63417fe
Gavin Shepherd <Gavinshepherd6688@gmail.com>
2017-03-05 20:20:09 -0500

    should be fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,26 +8,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Planet plant1= new Planet();
+        Planet planet1= new Planet();
         // todo: create a new variable "planet1" and set it to a new instance of the Planet class
-        plant1.name="Mercury";
-        plant1.distanceFromSun=.04;
-        plant1.satellites=0;
-        plant1.supportsLife=false;
+        planet1.name="Mercury";
+        planet1.distanceFromSun=.04;
+        planet1.satellites=0;
+        planet1.supportsLife=false;
         // todo: set all four properties of the Planet class on the planet1 instance: name, distanceFromSun, satellites, and supportsLife.
-        Planet plant2= new Planet();
+        Planet planet2= new Planet();
         // todo: create another new variable named "planet2" and set it to a new instance of the Planet class
-        plant2.name="Earth";
-        plant2.distanceFromSun=1;
-        plant2.satellites=1;
-        plant2.supportsLife=true;
+        planet2.name="Earth";
+        planet2.distanceFromSun=1;
+        planet2.satellites=1;
+        planet2.supportsLife=true;
         // todo: set all four properties of the Planet class on the planet2 instance
-        System.out.println("Name: "+plant1.name+" distanceFromSun: "+plant1.distanceFromSun+" satellites: "+plant1.satellites+" supportsLife: "+plant1.supportsLife);
+        System.out.println("Name: "+planet1.name+" distanceFromSun: "+planet1.distanceFromSun+" satellites: "+planet1.satellites+" supportsLife: "+planet1.supportsLife);
         // todo: Use println() to print a String description of planet1 using string concatenation
         /*
             Example output: "Name: Earth, Distance from Sun: 1 AU, Satellites: 1 moon(s), Supports Life: true"
          */
-        System.out.println("Name: "+plant2.name+" distanceFromSun: "+plant2.distanceFromSun+" satellites: "+plant2.satellites+" supportsLife: "+plant2.supportsLife);
+        System.out.println("Name: "+planet2.name+" distanceFromSun: "+planet2.distanceFromSun+" satellites: "+planet2.satellites+" supportsLife: "+planet2.supportsLife);
         // todo: Use println() to print a String description of planet2, just like above
 
         /*
@@ -35,11 +35,11 @@ public class Main {
             changed. This change impacts only that one property on that one
             instance of the object.
          */
-        plant1.satellites=3;
+        planet1.satellites=3;
         // todo: change a single property on your planet1 instance
-        plant2.supportsLife=false;
+        planet2.supportsLife=false;
         // todo: change a different property on your planet2 instance
-        System.out.println("Name: "+plant1.name+" distanceFromSun: "+plant1.distanceFromSun+" satellites: "+plant1.satellites+" supportsLife: "+plant1.supportsLife);
+        System.out.println("Name: "+planet1.name+" distanceFromSun: "+planet1.distanceFromSun+" satellites: "+planet1.satellites+" supportsLife: "+planet1.supportsLife);
         // todo: Use println() to print a String description of planet1 again.
         /*
             Note that the property you changed on planet1 is reflected in the
@@ -47,7 +47,7 @@ public class Main {
             shows that these two instances are distinct and separate from each
             other.
          */
-        System.out.println("Name: " +plant2.name+" distanceFromSun: "+plant2.distanceFromSun+" satellites: "+plant2.satellites+" supportsLife: "+plant2.supportsLife);
+        System.out.println("Name: " +planet2.name+" distanceFromSun: "+planet2.distanceFromSun+" satellites: "+planet2.satellites+" supportsLife: "+planet2.supportsLife);
         // todo: Use println() to print a String description of planet2, again.  Note that the property you changed on planet2 is reflected in the output.
 
     }
